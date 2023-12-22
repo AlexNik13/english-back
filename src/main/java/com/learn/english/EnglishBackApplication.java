5e538ca59942e81bacf4ec9bb7b7f68ea315b8f1
AlexNik <4pnav@gmail.com>
2023-12-22 19:18:29 +0200

    Initial commit

---
+++ src/main/java/com/learn/english/EnglishBackApplication.java
@@ -0,0 +1,13 @@
+package com.learn.english;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnglishBackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EnglishBackApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/learn/english/EnglishBackApplicationTests.java
@@ -0,0 +1,13 @@
+package com.learn.english;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnglishBackApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
